6958b04293a5a4fb86d2b01cc4e4c4b9de81b8ea
jeanx353 <jeanx353@gmail.com>
2025-03-15 11:42:09 -0300

    primeiro

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeS = [];
+listaAmigos(nomeS);
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo === '') {
+        alert("Por favor, insira um nome!");
+        return;
+    } else {
+        nomeS.push(amigo);
+        listaAmigos();
+        document.getElementById('amigo').value = '';
+    }
+}
+
+function listaAmigos() {
+    let lista = document.getElementById('listaAmigos'); 
+    lista.innerHTML = '';
+    nomeS.forEach(amigo => {
+        let item = document.createElement('li');
+        item.textContent = amigo;
+        lista.appendChild(item);
+    });
+}
+
+function sortearAmigo() {
+    if (nomeS.length === 0) {
+        alert('Não há amigos disponíveis');
+        return;
+    } else {
+        let indiceAleatorio = Math.floor(Math.random() * nomeS.length);
+        let amigoSorteado = nomeS[indiceAleatorio];
+        document.getElementById('resultado').innerHTML = `O amigo sorteado foi: ${amigoSorteado}`;
+    }
+    let limparLista = document.getElementById("listaAmigos");
+    limparLista.innerHTML = "";
+    nomeS = [];
+}
